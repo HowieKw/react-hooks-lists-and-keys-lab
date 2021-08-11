0588271bba49b3f2f6b032d81f7f0434ba7c02fa
Howard Kwon <howie.ykwon70@gmail.com>
2021-08-11 18:32:44 -0400

    commit tree 658d112a000c9b76b695de82eb091a8b7ea70919

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const NavLinks = links.map((bar) => {
+    return <a href={`#${bar}`} key={bar}>{bar}</a>;
+  });
+
+  return <nav> {NavLinks} </nav>;
 }
 
 export default NavBar;
